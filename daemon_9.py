516c55c7c69d861165682c7817e2d12dcabbc1d9
Xingyan Chen <chenxingyan@bupt.edu.cn>
2021-10-18 15:40:32 +0800

    modified faster crawler

--- daemon_9.py
+++ daemon_9.py
@@ -1,5 +1,5 @@
 import os
 
 while 1:
-    run_depth_pct = 'python crawler.py --break-point=61684020884484b96b11add1 --user-id=616bb9c9b5b6d832ad606b2f'
+    run_depth_pct = 'python fast_crawler.py --break-point=61684020884484b96b11add1 --user-id=616bb9c9b5b6d832ad606b2f'
     os.system(run_depth_pct)
