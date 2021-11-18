2d89fd4eae404c9302f9afe410034e9a3b611df2
zhangxs <zhangxs7@smail.swufe.edu.cn>
2021-11-18 22:47:47 +0800

    add daemon

--- daemon_9.py
+++ daemon_9.py
@@ -1,5 +1,13 @@
 import os
-
+import time
+from database.conn_mongoDB import get_usful_count
+count = get_usful_count()
+print(count)
 while 1:
-    run_depth_pct = 'python fast_crawler.py --break-point=61684020884484b96b11add1 --user-id=616bb9c9b5b6d832ad606b2f'
-    os.system(run_depth_pct)
+    while count:
+
+        run_depth_pct = 'python crawler_by_cookie.py --break-point=61684020884484b96b11add1 --user-id=616bb9c9b5b6d832ad606b2f'
+        os.system(run_depth_pct)
+        count = get_usful_count()
+    time.sleep(300)
+    count = get_usful_count()
