49cb90c22e24fff21f803d1d2e22c1a5ddd2c652
zhangxs <zhangxs7@smail.swufe.edu.cn>
2021-11-18 20:22:37 +0800

    print count

--- daemon_2.py
+++ daemon_2.py
@@ -6,7 +6,7 @@ print(count)
 while 1:
     while count:
         run_depth_pct = 'python crawler_by_cookie.py --break-point=6167f1d5227d6c6a3647abe6 --user-id=6167ebb1227d6c6a3647abcf'
-        os.system(run_depth_pct)prin
+        os.system(run_depth_pct)
         count = get_usful_count()
     time.sleep(300)
     count = get_usful_count()
