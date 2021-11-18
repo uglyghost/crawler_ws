8b28fa3ed52155c2bc55315ca2fe1693a8df1516
zhangxs <zhangxs7@smail.swufe.edu.cn>
2021-11-18 20:22:41 +0800

    print count

--- daemon_3.py
+++ daemon_3.py
@@ -1,11 +1,14 @@
 import os
 import time
 from database.conn_mongoDB import get_usful_count
+
 count = get_usful_count()
+print(count)
 while 1:
     while count:
         run_depth_pct = 'python crawler_by_cookie.py --break-point=61684020884484b96b11add9 --user-id=6168218d884484b96b11adb8'
         os.system(run_depth_pct)
         count = get_usful_count()
     time.sleep(300)
+
     count = get_usful_count()
