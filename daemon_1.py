ef3de58f4f56f8a9ee8c97e3acf1d4eefba7e627
zhangxs <zhangxs7@smail.swufe.edu.cn>
2021-11-18 20:21:51 +0800

    print count

--- daemon_1.py
+++ daemon_1.py
@@ -2,6 +2,7 @@ import os
 import time
 from database.conn_mongoDB import get_usful_count
 count = get_usful_count()
+print(count)
 while 1:
     while count:
         run_depth_pct = 'python crawler_by_cookie.py --break-point=615ff07c1592e4bbdcfc1eb2 --user-id=616cc7d0b5b6d832ad606b46'
--- daemon_4.py
+++ daemon_4.py
@@ -2,6 +2,7 @@ import os
 import time
 from database.conn_mongoDB import get_usful_count
 count = get_usful_count()
+print(count)
 while 1:
     while count:
         run_depth_pct = 'python crawler_by_cookie.py --break-point=61684020884484b96b11add0 --user-id=616bbb13b5b6d832ad606b32'
