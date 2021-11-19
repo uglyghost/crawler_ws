bffad49d90a8bf9bdd78f59503f3e444cb95b47a
zhangxs <zhangxs7@smail.swufe.edu.cn>
2021-11-19 21:21:22 +0800

    add count info

--- daemon_getcookie.py
+++ daemon_getcookie.py
@@ -1,4 +1,15 @@
 import os
+import time
+
 while 1:
-    run_depth_pct = 'python get_cooies.py'
-    os.system(run_depth_pct)
+    count = 4
+    t1 = time.time()
+    while count:
+        run_depth_pct = 'python get_cooies.py'
+        os.system(run_depth_pct)
+        count-=1
+    t2 = time.time()
+    det = t2-t1
+    if det <120:
+        print('2分钟内连续4此请求失败，休眠3分钟')
+        time.sleep(180)
